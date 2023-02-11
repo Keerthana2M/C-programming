6782ce2467bd8684bd3c29c54abeb1f273929a70
Keerthana.M <124800769+Keerthana2M@users.noreply.github.com>
2023-02-11 19:33:06 +0530

    done

---
+++ vowel.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+char alpha;
+printf("enter a character\n");
+scanf("%c",&alpha);
+switch(alpha)
+{
+case'a':
+	printf("vowel");
+	break;
+case'e':
+	printf("vowel");
+	break;
+case'i':
+	printf("vowel");
+	break;
+case'o':
+	printf("vowel");
+	break;
+case'u':
+	printf("vowel");
+	break;
+default:
+	printf("consonant");
+}
+}
